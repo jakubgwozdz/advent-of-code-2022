a0e18d6b53f52926c7ffe92dd60860ef9106f313
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2022-12-03 05:48:11 +0100

    Day 2

--- src/main/kotlin/day2/Day2.kt
+++ src/main/kotlin/day2/Day2.kt
@@ -1,6 +1,7 @@
+@file:Suppress("USELESS_CAST")
+
 package day2
 
-import parseRecords
 import readAllText
 import kotlin.time.DurationUnit
 import kotlin.time.measureTime
@@ -10,11 +11,39 @@ fun main() = measureTime {
     println(part2(readAllText("local/day2_input.txt")))
 }.let { println(it.toString(DurationUnit.SECONDS, 3)) }
 
-private val regex = "(.+)".toRegex()
-private fun parse(matchResult: MatchResult) = matchResult.destructured.let { (a) -> a }
+fun part1(input: String) = input.lineSequence().filterNot(String::isBlank).sumOf(::part1result)
+fun part2(input: String) = input.lineSequence().filterNot(String::isBlank).sumOf(::part2result)
 
-fun part1(input: String) = input.parseRecords(regex, ::parse)
-    .count()
+const val A = 1
+const val B = 2
+const val C = 3
 
-fun part2(input: String) = input.parseRecords(regex, ::parse)
-    .count()
+const val LOSE = 0
+const val DRAW = 3
+const val WIN = 6
+
+private fun part1result(it: String) = when (it) {
+    "A X" -> A + DRAW
+    "B X" -> A + LOSE
+    "C X" -> A + WIN
+    "A Y" -> B + WIN
+    "B Y" -> B + DRAW
+    "C Y" -> B + LOSE
+    "A Z" -> C + LOSE
+    "B Z" -> C + WIN
+    "C Z" -> C + DRAW
+    else -> error("WTF `$it`")
+}
+
+private fun part2result(it: String) = when (it) {
+    "A X" -> C + LOSE
+    "B X" -> A + LOSE
+    "C X" -> B + LOSE
+    "A Y" -> A + DRAW
+    "B Y" -> B + DRAW
+    "C Y" -> C + DRAW
+    "A Z" -> B + WIN
+    "B Z" -> C + WIN
+    "C Z" -> A + WIN
+    else -> error("WTF `$it`")
+}
