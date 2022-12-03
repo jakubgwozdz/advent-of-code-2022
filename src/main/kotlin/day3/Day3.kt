72f807aadb6bead1e5c7b5f7a9c79b965432f430
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2022-12-03 21:35:40 +0100

    Day 3 idiomatic and readable

--- src/main/kotlin/day3/Day3.kt
+++ src/main/kotlin/day3/Day3.kt
@@ -10,13 +10,13 @@ fun main() = measureTime {
 }.let { println(it.toString(DurationUnit.SECONDS, 3)) }
 
 fun part1(input: String) = input.lineSequence().filter(String::isNotBlank)
-    .map { it.chunked(it.length / 2) }
-    .map { it.map(String::toSet).reduce(Set<Char>::intersect).single() }
+    .map { it.chunked(it.length / 2).map(String::toSet) }
+    .map { (p1, p2) -> p1.first { it in p2 } }
     .sumOf(::priority)
 
 fun part2(input: String) = input.lineSequence().filter(String::isNotBlank)
-    .chunked(3)
-    .map { it.map(String::toSet).reduce(Set<Char>::intersect).single() }
+    .chunked(3).map { it.map(String::toSet) }
+    .map { (p1, p2, p3) -> p1.first { it in p2 && it in p3 } }
     .sumOf(::priority)
 
 private fun priority(c: Char) = when {
