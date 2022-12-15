0185458885e183abf97b81eff02c31662c2f242f
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2022-12-15 13:51:29 +0100

    day 15 smart

--- src/main/kotlin/day15/Day15.kt
+++ src/main/kotlin/day15/Day15.kt
@@ -22,6 +22,15 @@ tailrec fun LongRange.splitRange(
         )
     }
 
+fun <T> List<T>.merge(predicate: (T, T) -> Boolean, mergeOp: (T, T) -> T): List<T> =
+    fold<T, MutableList<T>>(mutableListOf()) { acc, t ->
+        acc.apply {
+            if (isEmpty()) add(t)
+            else if (predicate(last(), t)) add(mergeOp(removeLast(), t))
+            else add(t)
+        }
+    }.toList()
+
 fun part1(input: String): Long {
     val data = input.parseRecords(regex, ::parse).toList()
     val testLine = if (data.first().first.first == 2L) 10L else 2000000L
@@ -35,15 +44,15 @@ fun part1(input: String): Long {
             val (bx, by) = beacon
             val dist = (sx - bx).absoluteValue + (sy - by).absoluteValue
             val dist10 = dist - (sy - testLine).absoluteValue
-            sx - dist10 to sx + dist10
+            sx - dist10..sx + dist10
         }
-        .filter { (s, e) -> s <= e }
-        .sortedWith(compareBy<Pair<Long, Long>> { it.first }.thenBy { it.second })
+        .filter { it.first <= it.last }
+        .sortedWith(compareBy<LongRange> { it.first }.thenBy { it.last })
 
-    val cuts = buildSet { ranges.forEach { (s, e) -> add(s); add(e) } }
+    val cuts = buildSet { ranges.forEach { add(it.first); add(it.last) } }
         .toList().sorted()
 
-    return ranges.flatMap { (s, e) -> (s..e).splitRange(cuts) }
+    return ranges.flatMap { it.splitRange(cuts) }
         .map { it.first to it.last }
         .filter { (s, e) -> s <= e }
         .toSet().sumOf { (s, e) -> e - s + 1 } - beaconsAlreadyOnTestLine
@@ -51,31 +60,59 @@ fun part1(input: String): Long {
 
 fun part2(input: String): Long {
     val data = input.parseRecords(regex, ::parse).toList()
-    val max = if (data.first().first.first == 2L) 20L else 4000000L
-    return (0..max).asSequence().map { y ->
-        val ranges = data
-            .map { (sensor, beacon) ->
-                val (sx, sy) = sensor
-                val (bx, by) = beacon
-                val dist = (sx - bx).absoluteValue + (sy - by).absoluteValue
-                val dist10 = dist - (sy - y).absoluteValue
-                sx - dist10 to sx + dist10
-            }
-            .filter { (s, e) -> s <= e }
-            .sortedWith(compareBy<Pair<Long, Long>> { it.first }.thenBy { it.second })
+    val sensors = data.map { (sensor, beacon) ->
+        val (sx, sy) = sensor
+        val (bx, by) = beacon
+        val dist = (sx - bx).absoluteValue + (sy - by).absoluteValue
+        sensor to dist
+    }
 
-        if (ranges.first().first > 0) return@map y to listOf(0L)
+    val sensors45 = sensors.map { (s, r) ->
+        val (x0, y0) = s
+        val x1 = x0 - y0
+        val y1 = x0 + y0
+        x1 to y1 to r
+    }
 
-        var x = ranges.first().second + 1
+    val ranges45 = sensors45.map { (s, d) ->
+        val (x, y) = s
+        (x - d..x + d) to (y - d..y + d)
+    }
 
-        for (r in ranges)
-            if (r.first > x) return@map y to listOf(x)
-            else if (r.second + 1 > x) x = r.second + 1
+    val horizCuts45 = buildSet { ranges45.forEach { (h, v) -> add(h.first); add(h.last) } }
+        .toList().sorted()
 
-        return@map y to emptyList()
+    val vertCuts45 = buildSet { ranges45.forEach { (h, v) -> add(v.first); add(v.last) } }
+        .toList().sorted()
 
-    }.first { (_, l) -> l.isNotEmpty() }
-        .let { (y, l) -> y + l.single() * 4000000 }
+    val blocks45: List<Pair<LongRange, List<LongRange>>> = ranges45.flatMap { (h, v) ->
+        h.splitRange(horizCuts45).flatMap { hh -> v.splitRange(vertCuts45).map { vv -> hh to vv } }
+    }
+        .asSequence()
+        .sortedWith(compareBy<Pair<LongRange, LongRange>> { it.first.first }.thenBy { it.first.last }
+            .thenBy { it.second.first }.thenBy { it.second.last })
+        .groupBy { it.first }.map { (h, l) -> h to l.map { it.second } }
+        .map { (h, vl) ->
+            h to vl.merge(
+                predicate = { a, b -> a.last >= b.first - 1 },
+                mergeOp = { a, b -> a.first..b.last }
+            )
+        }
+        .merge(
+            predicate = { (a, al), (b, bl) -> a.last >= b.first - 1 && al == bl },
+            mergeOp = { (a, al), (b, bl) -> a.first..b.last to al }
+        )
+
+    val invalid45 = blocks45.first { it.second.size > 1 }
+        .let { (h, vl) -> h.single() to vl.first().last + 1 }
+
+    val invalid = invalid45.let { (x1, y1) ->
+        val x0 = (x1 + y1) / 2
+        val y0 = x0 - x1
+        x0 to y0
+    }
+
+    return invalid.let { (x, y) -> y + x * 4000000 }
 }
 
 private val regex = "Sensor at x=(-?\\d+), y=(-?\\d+): closest beacon is at x=(-?\\d+), y=(-?\\d+)".toRegex()
@@ -104,5 +141,5 @@ fun main() {
     execute(::part1, test, 26)
     execute(::part1, input, 5525847)
     execute(::part2, test, 56000011)
-    execute(::part2, input)
+    execute(::part2, input, 13340867187704)
 }
