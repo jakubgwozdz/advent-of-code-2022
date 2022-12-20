6ca13bca1d4eb02969316fd553c9410db376218e
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2022-12-20 11:39:58 +0100

    day 20 p2 ugly af

--- src/main/kotlin/day20/Day20.kt
+++ src/main/kotlin/day20/Day20.kt
@@ -3,47 +3,61 @@ package day20
 import execute
 import readAllText
 
+private class Elem(val v: Long) {
+    override fun toString() = v.toString()
+}
+
 fun part1(input: String) = parse(input)
+    .map { Elem(it * 1L) }
     .let { initial ->
         var result = initial
-        initial.forEach { n ->
-            result = result.process(n)
+        initial.forEach {
+            result = result.process(it)
         }
 
-        result
-    }
-    .let {
-        val offset = it.indexOf(0)
-        Triple(it[(offset + 1000) % it.size], it[(offset + 2000) % it.size], it[(offset + 3000) % it.size])
+        val offset = result.withIndex().single { it.value.v == 0L }.index
+        val first = (offset + 1000) % result.size
+        val second = (offset + 2000) % result.size
+        val third = (offset + 3000) % result.size
+        Triple(result[first], result[second], result[third])
     }
     .also { println(it) }
-    .let { (a, b, c) -> a + b + c }
+    .let { (a, b, c) -> a.v + b.v + c.v }
+
 
 private fun parse(input: String) = input.lineSequence().mapNotNull { it.toIntOrNull() }
     .toList()
 
-private fun List<Int>.process(n: Int): List<Int> = if (n % (size - 1) != 0) {
-    val oldIdx = indexOf(n)
-    var newIdx = oldIdx + n
-    while (newIdx < 0) newIdx += size - 1
-    while (newIdx >= size) newIdx -= size - 1
-    val newResult = if (newIdx > oldIdx) {
-        val start = subList(0, oldIdx)
-        val mid = subList(oldIdx + 1, newIdx + 1)
-        val end = subList(newIdx + 1, size)
-        start + mid + n + end
-    } else {
-        val start = subList(0, newIdx)
-        val mid = subList(newIdx, oldIdx)
-        val end = subList(oldIdx + 1, size)
-        start + n + mid + end
-    }
-//                println("$n: $oldIdx->$newIdx : $result -> $newResult")
-    newResult
-} else this
+private fun List<Elem>.process(e: Elem): List<Elem> {
+    val oldIdx = withIndex().single { it.value == e }.index
+    val newResult = subList(oldIdx + 1, size) + subList(0, oldIdx) + subList(oldIdx + 1, size) + subList(0, oldIdx)
+
+    var delta = (e.v % (size - 1)).toInt()
+    while (delta >= 0) delta -= size - 1
+    while (delta < 0) delta += size - 1
+
+    return newResult.subList(delta, delta + size - 1) + e
+}
 
 fun part2(input: String) = parse(input)
-    .count()
+    .map { Elem(it * 811589153L) }
+    .let { initial ->
+        var result = initial
+        repeat(10) {
+            initial.forEach {
+                result = result.process(it)
+            }
+        }
+
+        val offset = result.withIndex().single { it.value.v == 0L }.index
+        val first = (offset + 1000) % result.size
+        val second = (offset + 2000) % result.size
+        val third = (offset + 3000) % result.size
+        Triple(result[first], result[second], result[third])
+    }
+    .also { println(it) }
+    .let { (a, b, c) -> a.v + b.v + c.v }
+
 
 fun main() {
     val input = readAllText("local/day20_input.txt")
@@ -57,10 +71,10 @@ fun main() {
         4
     """.trimIndent()
 
-    println(listOf(0, -22, 2, 3, 4, 5, 6, 7, 8, 9).process(-22))
+//    println(listOf(0, -22, 2, 3, 4, 5, 6, 7, 8, 9).process(-22))
 
     execute(::part1, test, 3)
-    execute(::part1, input)
-    execute(::part2, test)
+    execute(::part1, input, 8764)
+    execute(::part2, test, 1623178306)
     execute(::part2, input)
 }
