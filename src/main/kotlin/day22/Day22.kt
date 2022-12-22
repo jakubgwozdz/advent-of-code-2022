38ce4a265baca49076c854e3d5ae4bdaf57c835e
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2022-12-22 15:58:54 +0100

    day 22 p1

--- src/main/kotlin/day22/Day22.kt
+++ src/main/kotlin/day22/Day22.kt
@@ -24,28 +24,43 @@ fun part2(input: String) = input.split("\n\n")
     .let { (board, path) -> path.fold(Point(1, 1) to 0, board::playPart2) }
     .let { (pos, dir) -> pos.first * 1000 + pos.second * 4 + dir }
 
-private fun Board.playPart1(from: Pair<Point, Int>, op: Op): Pair<Point, Int> {
-    var (pos1, dir1) = from
+private fun Board.playPart1(from: Pair<Point, Int>, op: Op) =
+    play(from, op, ::stepPart1)
+
+private fun Board.playPart2(from: Pair<Point, Int>, op: Op) =
+    play(from, op, ::stepPart2)
+
+private fun Board.play(from: Pair<Point, Int>, op: Op, step: (Pair<Point, Int>) -> Pair<Point, Int>): Pair<Point, Int> =
     when (op) {
-        Turn.L -> dir1 = (dir1 + 3) % 4
-        Turn.R -> dir1 = (dir1 + 1) % 4
-        else -> repeat((op as Move).steps) {
-            val (r, c) = pos1
-            val posX = when (dir1) {
-                0 -> r to (c + 1).keepIn(rows[r]!!)
-                1 -> (r + 1).keepIn(cols[c]!!) to c
-                2 -> r to (c - 1).keepIn(rows[r]!!)
-                3 -> (r - 1).keepIn(cols[c]!!) to c
-                else -> wtf(dir1)
-            }
-            if (posX !in walls) pos1 = posX
+        Turn.L -> from.first to (from.second + 3) % 4
+        Turn.R -> from.first to (from.second + 1) % 4
+        is Move -> (1..op.steps).fold(from) { acc, _ ->
+            val fromX = step(acc)
+            if (fromX.first !in walls) fromX else acc
         }
     }
-    return pos1 to dir1
+
+private fun Board.stepPart1(from: Pair<Point, Int>): Pair<Point, Int> = from.let { (pos, dir) ->
+    val (r, c) = pos
+    when (dir) {
+        0 -> r to (c + 1).keepIn(rows[r]!!)
+        1 -> (r + 1).keepIn(cols[c]!!) to c
+        2 -> r to (c - 1).keepIn(rows[r]!!)
+        3 -> (r - 1).keepIn(cols[c]!!) to c
+        else -> wtf(dir)
+    } to dir
 }
 
-private fun Board.playPart2(from: Pair<Point, Int>, op: Op): Pair<Point, Int> {
+private fun Board.stepPart2(from: Pair<Point, Int>): Pair<Point, Int> = from.let { (pos, dir) ->
+    val (r, c) = pos
     TODO()
+    when (dir) {
+        0 -> r to (c + 1).keepIn(rows[r]!!)
+        1 -> (r + 1).keepIn(cols[c]!!) to c
+        2 -> r to (c - 1).keepIn(rows[r]!!)
+        3 -> (r - 1).keepIn(cols[c]!!) to c
+        else -> wtf(dir)
+    } to dir
 }
 
 private fun parseBoard(
@@ -70,7 +85,7 @@ private fun parseBoard(
         .filterNot { (_, r) -> r.isEmpty() }
         .toMap()
     val board = Board(rows, cols, walls)
-    val path = buildList<Op> {
+    val path = buildList {
         pathData.trimEnd().forEach {
             if (it.isDigit() && this.lastOrNull() is Move) add(Move((removeLast() as Move).steps * 10 + it.digitToInt()))
             else add(
